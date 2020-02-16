e0d834db5ef005cccbeba9561979b0304af274ff
小年轻只爱她 <490424984@qq.com>
2020-02-16 22:35:16 +0800

    接入blinker

---
+++ 接入blinker平台/接入blinker/接入blinker.ino
@@ -0,0 +1,49 @@
+#define BLINKER_PRINT Serial
+#define BLINKER_WIFI
+
+#include <Blinker.h>
+/*******此处修改*******/
+char auth[] = "7f9a3730be92";//秘钥
+char ssid[] = "MI";//WiFi名称
+char pswd[] = "asdfghjkl";//WiFi密码
+
+// 新建组件对象
+BlinkerButton Button1("key");
+BlinkerNumber Number1("num-abc");
+
+int counter = 0;
+
+// 按下按键即会执行该函数
+void button1_callback(const String & state) {
+    BLINKER_LOG("get button state: ", state);
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+// 如果未绑定的组件被触发，则会执行其中内容
+void dataRead(const String & data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+    counter++;
+    Number1.print(counter);
+}
+
+void setup() {
+    // 初始化串口
+    Serial.begin(115200);
+
+    #if defined(BLINKER_PRINT)
+        BLINKER_DEBUG.stream(BLINKER_PRINT);
+    #endif
+
+    // 初始化有LED的IO
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    // 初始化blinker
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+    Button1.attach(button1_callback);//注册按键回调函数
+}
+
+void loop() {
+    Blinker.run();
+}
